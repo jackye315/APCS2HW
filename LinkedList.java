5c5bdf66914264966fa4e0549129259af23ccf03
Jack Ye <jackye315@gmail.com>
2015-03-05 23:07:47 -0800

    Confused

--- LinkedList.java
+++ LinkedList.java
@@ -1,16 +1,63 @@
 public class LinkedList {
 
     private LNode front;
-    public LinkdList(LNode node){
+    public LinkedList(LNode node){
 	front=node;
     }
 
     public String toString(){
-	return front.toString;
+	return ""+front;
     }
 
     public void add(char c){
-
-
+	front.getNext()=new LNode(c);
     }
+
+    public void add(char c, int i){
+	while(i>0){
+	    front.getNext()=front;
+	    i=i-1;
+	}
+	front.getNext=new LNode(c);
+    }
+    public char get(int i){
+	while(i>0){
+	    front.getNext=front;
+	    i=i-1;
+	}
+	return front.getData();
+    }
+
+    public void set(char c, int i){
+	while(i>0){
+	    front.getNext=front;
+	    i=i-1;
+	}
+	front.getNext.setData(c);
+    }
+
+    public char remove(int i){
+	while(i>0){
+	    front.Next=front;
+	    i=i-1;
+	}
+	char removed=front.next.getData;
+	front.getNext.getNext=front;
+	return removed;
+    }
+
+    public int size(){
+	int ans=0;
+	while(front.next != null){
+	    ans=ans+1;
+	}
+	return ans;
+    }
+
+public static void main(String[] args) {
+     LinkedList List=new LinkedList(front);
+    System.out.println(List.size());
+
 }
+}
+//I'm confused about how to access the Next node.

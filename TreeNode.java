e4bccfe198e43f9c1ac1b831085a2bdfa1cf5d79
jackye315 <jackye315@gmail.com>
2015-04-14 14:50:20 -0400

    Tree

---
+++ TreeNode.java
@@ -0,0 +1,30 @@
+public class TreeNode <E> {
+    
+    private E data;
+    private TreeNode<E> left;
+    private TreeNode<E> right;
+    public TreeNode ( E d ) {
+	data = d;
+    }
+    public String toString() {
+	return "" + data;
+    }
+    public void setData( E c ) {
+	data = c;
+    }
+    public void setRight( TreeNode<E> n ) {
+	right = n;
+    }
+    public void setLeft(TreeNode<E> n){
+	left=n;
+    }
+    public E getData() {
+	return data;
+    }
+    public TreeNode<E> getRight() {
+	return right;
+    }
+    public TreeNode<E> getLeft(){
+	return left;
+    }
+}
